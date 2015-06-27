c8492f797e3c0fa80883ebd99a401aca5c04e358
Erik <ehernandez@icstars.org>
2015-06-27 00:07:13 -0500

    text,links, map  in index.html

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,16 +41,16 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(41.9001607,-87.707817), // Chicago
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
-        draggable: false,
+        draggable: true,
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
+       /** styles: [{
             "featureType": "water",
             "elementType": "geometry",
             "stylers": [{
@@ -157,7 +157,7 @@ function init() {
             }, {
                 "weight": 1.2
             }]
-        }]
+        }]**/
     };
 
     // Get the HTML DOM element that will contain your map 
